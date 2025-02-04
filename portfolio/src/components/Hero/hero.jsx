689170ebd8f4119d3f2486630c6dc1f38102956e
abineshk02 <abineshk213@gmail.com>
2025-02-04 07:04:14 +0530

    hero section

--- portfolio/src/components/Hero/hero.jsx
+++ portfolio/src/components/Hero/hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
     <div className='hero'>
         <img src={coding} alt="" />
 
-        <h1> I&apos;m Abinesh, Associate Python FullStack Developer</h1>
-        <p> I am an Associate Python Full Stack Developer based in Chennai, with over 10 months of experience. I have worked at Code Crave Solutions, a dynamic startup, where I gained hands-on experience in developing and maintaining full-stack applications.</p>
-        <div className="hero-section">
+        <h1> <span>I&apos;m Abinesh,</span> Associate Python FullStack Developer</h1>
+        <p> I am an Associate Python Full Stack Developer based in Chennai, with over 10 months of experience. I have worked at Code Crave Solutions, a dynamic startup.</p>
+        <div className="hero-action">
             <div className="hero-connect">Connect with me</div>
             <div className="hero-resume">My resume</div>
         </div>
